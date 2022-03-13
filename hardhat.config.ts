b1f7e5ffc1d361e8a0fcaf9c39f632fe27adecee
Irsan <irsantyohadi@gmail.com>
2022-03-13 21:05:23 +0700

    add testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,51 @@
-import 'hardhat-deploy';
-import '@nomiclabs/hardhat-ethers';
 import '@typechain/hardhat';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-ethers';
+import 'hardhat-gas-reporter';
+import 'dotenv/config';
+import 'solidity-coverage';
+import 'hardhat-deploy';
+import 'solidity-coverage';
 import { HardhatUserConfig } from 'hardhat/config';
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || '';
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL ||
+  'https://eth-rinkeby.alchemyapi.io/v2/XJR316km5_b4RQCJrwa48UMIB2SHApRm';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || 'privatKey';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+
 const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
-      chainId: 31337,
+      chainId: 1337,
     },
     localhost: {
-      chainId: 31337,
+      chainId: 1337,
     },
+    // rinkeby: {
+    //   url: RINKEBY_RPC_URL,
+    //   accounts: [PRIVATE_KEY],
+    //   chainId: 4,
+    // },
   },
-  solidity: '0.8.10',
+  solidity: '0.8.9',
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    outputFile: 'gas-report.txt',
+    noColors: true,
+  },
   namedAccounts: {
     deployer: {
-      default: 0,
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
     },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
   },
 };
 
